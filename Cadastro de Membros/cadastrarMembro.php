6fe95fbe1377cecff4e63872c2d6b237f9566a4f
Davi-Camargo-Souza <camargod86@gmail.com>
2023-11-22 20:26:21 -0300

    adicionado funcionalidade de cadastro de tecnicos

--- Cadastro de Membros/cadastrarMembro.php
+++ Cadastro de Membros/cadastrarMembro.php
@@ -7,13 +7,16 @@
     $categoria = $_POST["categoria"];
     $data = date("Y-m-d");
 
-    if ($funcao == "Jogador") {
-        if ($categoria == "1"){
-            $numero = 1;
-        } else {
-            $numero = $_POST["numero"];
-        }
-    };
+    $sql = "SELECT 1 FROM cadastromembros.membros
+            WHERE cpf = '{$documento}'";
+
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        print "<script>alert('CPF já cadastrado como um jogador.')</script>";
+        print "<script>location.href='cadastromembros.php'</script>";
+        exit();
+    }
 
     $sql = "INSERT INTO cadastromembros.membros
             (nome, cpf, dtaNasc, codFuncao)
@@ -23,17 +26,23 @@
     $conn->query($sql);
     $codMembro = $conn->insert_id;
 
-    $sql = "INSERT INTO cadastromembros.cadastro_jogadores
+    if ($funcao == "Jogador") {
+        if ($categoria == "1"){
+            $numero = 1;
+        } else {
+            $numero = $_POST["numero"];
+        }
+        $sql = "INSERT INTO cadastromembros.cadastro_jogadores
             (codMembro, numero, dtaCadastro) VALUES ('{$codMembro}','{$numero}', '{$data}')";
-    
-    $result = $conn->query($sql);
+    } else if ($funcao == "Tecnico"){
+        $sql = "INSERT INTO cadastromembros.cadastro_comissao_tecnica
+                (codMembro, dtaCadastro) VALUES ('{$codMembro}','{$data}')";
+    };
 
+    $result = $conn->query($sql);
     if ($result === TRUE) {
         print"<script>alert('Registro cadastrado com sucesso.')</script>";
     }
 
     $conn->close();
-
-
-    
-
+    print "<script>location.href='cadastromembros.php'</script>";
