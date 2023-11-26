5ceb2af337dccb6091a3c180c211f93e743498af
Davi-Camargo-Souza <camargod86@gmail.com>
2023-11-26 19:59:42 -0300

    adicionado botão de sair na pagina inicial

--- Cadastro de Membros/dashboard.php
+++ Cadastro de Membros/dashboard.php
@@ -13,6 +13,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body class="corpo">
+    <div>
+        <button id="sair-btn" class="btn" onclick="sair()" style="background-color: darkred; color: white"><i class="fa-solid fa-right-to-bracket"></i></button>
+    </div>
     <main class="centralizar">
     <div class="container">
         <div class="mb-5" style="text-align: center; color: darkred">
@@ -38,6 +41,7 @@
         </div>
     </div>
     </main>
+    <script src="scripts/script.js"></script>
     <script src="https://kit.fontawesome.com/6f3da5c6b0.js" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
